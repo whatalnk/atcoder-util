27285fb3ca27b13907ba257786b627be40e98396
Wataru NAKAI <watnk0806@gmail.com>
2019-06-24 21:52:27 +0900

    update

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 // Submission JSON types
@@ -21,15 +24,7 @@ type Submission struct {
 	Language, Result string
 }
 
-// FileInfo types for files to be saved
-type FileInfo struct {
-	EpochSecond                    float64
-	ProblemID, ContestID, Language string
-}
-
-func main() {
-	// Get API
-	user := "wtnk0812"
+func fetchSubmissions(user string) map[int]Submission {
 	url := "https://kenkoooo.com/atcoder/atcoder-api/results?user=" + user
 	resp, err := http.Get(url)
 	if err != nil {
@@ -45,42 +40,145 @@ func main() {
 		log.Fatal(err)
 	}
 
-	m := make(map[int]FileInfo)
+	m := make(map[int]Submission)
 
 	for _, v := range data {
-		m[int(v.ID)] = FileInfo{
-			EpochSecond: v.EpochSecond,
-			ProblemID:   v.ProblemID,
-			ContestID:   v.ContestID,
-			Language:    v.Language,
+		m[v.ID] = v
+	}
+
+	return m
+}
+
+func normLang(l string) string {
+	var ret string
+	switch {
+	case strings.Contains(l, "Ruby"):
+		ret = "ruby"
+	case strings.Contains(l, "Python"):
+		ret = "python"
+	case strings.Contains(l, "C++"):
+		ret = "cpp"
+	case strings.Contains(l, "Go"):
+		ret = "go"
+	case strings.Contains(l, "Rust"):
+		ret = "rust"
+	default:
+		ret = "other"
+	}
+	return ret
+}
+
+func langToExt(l string) string {
+	var ret string
+	switch l {
+	case "ruby":
+		ret = ".rb"
+	case "python":
+		ret = ".py"
+	case "cpp":
+		ret = ".cpp"
+	case "go":
+		ret = ".go"
+	case "rust":
+		ret = "rs"
+	default:
+		ret = ".txt"
+	}
+	return ret
+}
+
+func checkAttribute(n *html.Node, key string) (string, bool) {
+	for _, attr := range n.Attr {
+		if attr.Key == key {
+			return attr.Val, true
 		}
 	}
+	return "", false
 
-	// save gob to local file
-	f, err := os.Create("submissions.gob")
+}
+
+func checkID(n *html.Node, key string) bool {
+	if n.Type == html.ElementNode {
+		v, ok := checkAttribute(n, "id")
+		if ok && v == key {
+			return true
+		}
+	}
+	return false
+}
+
+func walk(n *html.Node, key string) *html.Node {
+	if checkID(n, key) {
+		return n
+	}
+
+	for nn := n.FirstChild; nn != nil; nn = nn.NextSibling {
+		res := walk(nn, key)
+		if res != nil {
+			return res
+		}
+	}
+	return nil
+}
+
+func getElementByID(n *html.Node, key string) *html.Node {
+	return walk(n, key)
+}
+
+func fetchCode(contestID string, id int) string {
+	url := fmt.Sprintf("https://atcoder.jp/contests/%s/submissions/%d", contestID, id)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	submissionCode := getElementByID(doc, "submission-code")
+	ol := submissionCode.FirstChild
+	code := ol.Data
+	return code
+}
+
+func update(targetDir string, submissions map[int]Submission) {
+	for _, v := range submissions {
+		lang := normLang(v.Language)
+		ext := langToExt(lang)
+		fileName := fmt.Sprintf("%d%s", v.ID, ext)
+		filePath := filepath.Join(targetDir, lang, v.ContestID, v.ProblemID, fileName)
+		_, err := os.Stat(filePath)
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(filepath.Dir(filePath), 0777)
+			if err != nil {
+				log.Fatal(err)
+			}
+			code := fetchCode(v.ContestID, v.ID)
+			// fmt.Println(code)
+			save(filePath, code)
+		}
+		// for debug
+		break
+	}
+}
+
+func save(filePath string, code string) {
+	f, err := os.Create(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	enc := gob.NewEncoder(f)
-	err = enc.Encode(m)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Save: Done!")
+	f.WriteString(code)
+}
 
-	loaded := make(map[int]FileInfo)
-	f, err = os.Open("submissions.gob")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	dec := gob.NewDecoder(f)
-	err = dec.Decode(&loaded)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Load: Done!")
-	log.Println(loaded[5429957])
+func main() {
+	// Get API
+	user := "wtnk0812"
+	targetDir := "atcoder"
+	submissions := fetchSubmissions(user)
+	update(targetDir, submissions)
+	// log.Println(submissions[5429957])
 }
