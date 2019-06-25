563c04741e8b91cd2c638b0dd5dc06760e9a93d8
Wataru NAKAI <watnk0806@gmail.com>
2019-06-25 21:50:33 +0900

    Delete comment

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "github.com/whatalnk/atcoder-util/acucore"
 
 func main() {
-	// Get API
 	user := "wtnk0812"
 	targetDir := "atcoder"
 	submissions := acucore.FetchSubmissions(user)
 	acucore.Update(targetDir, submissions)
-	// log.Println(submissions[5429957])
 }
